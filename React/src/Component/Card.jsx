940c3704b998e3461e56b29c89c414c6d1ff2a2a
Shubham <shubhamagrahari9191@gmail.com>
2025-10-31 19:09:57 +0530

    adding react folder

---
+++ React/src/Component/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.css'
+export default function Card() {
+  return (
+    <div id='card'>
+<img src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" alt="" height={100} width={100} />
+<h1>abes</h1>
+<h1> b.tech</h1>
+    </div>
+    
+  )
+}
